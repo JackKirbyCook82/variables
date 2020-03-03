f1c6c8c23a1f83396967ce297db2cb15c3b9a79a
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-03-02 21:21:59 -0800

    Updated

--- community.py
+++ community.py
@@ -8,9 +8,7 @@ Created on Mon Feb 24 2020
 
 from collections import namedtuple as ntuple
 
-from specs import HistogramSpec
-
-from variables.variable import Variable, create_customvariable
+from variables.variable import Variable
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
@@ -22,11 +20,8 @@ __license__ = ""
 _aslist = lambda items: [items] if not isinstance(items, (list, tuple)) else list(items)
 _filterempty = lambda items: [item for item in _aslist(items) if item is not None]
 
-Race_Histogram = create_customvariable(HistogramSpec(data='race', categories=['White', 'Black', 'Native', 'Asian', 'Islander', 'Other', 'Mix']))
-Education_Histogram = create_customvariable(HistogramSpec(data='education', categories=['Uneducated', 'GradeSchool', 'Associates', 'Bachelors', 'Graduate']))
-LifeStage_Histogram = create_customvariable(HistogramSpec(data='lifestage', categories=['Baby', 'Toddler', 'Child', 'Teenager', 'YoungAdult', 'MidLife', 'Mature', 'Elder']))
 
-COMMUNITY = {'race':Race_Histogram, 'education':Education_Histogram, 'lifestage':LifeStage_Histogram}
+COMMUNITY = {}
 DELIMITER = '|'
 SEPARATOR = '&'
 CommunitySgmts = ntuple('CommunitySgmts', ' '.join(list(COMMUNITY.keys())))
